11a24d431799fabecd2e9308d057a758b9f8f48f
ruksat13 <mdruksathasanakib@gmail.com>
2026-05-28 20:45:10 +0600

    Configure CORS to allow requests from client origins

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "https://visionary-raindrop-12bd9e.netlify.app"],
+    credentials: true,
+}));
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
